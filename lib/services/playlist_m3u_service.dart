4e020231d328582f5ca3aaa00fad3c5276c38c5a
ximingxingyin <152784525@qq.com>
2026-08-10 19:33:51 +0800

    Add Value import to playlist_m3u_service.dart

--- lib/services/playlist_m3u_service.dart
+++ lib/services/playlist_m3u_service.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:drift/drift.dart' show Variable;
+import 'package:drift/drift.dart' show Variable, Value;
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
